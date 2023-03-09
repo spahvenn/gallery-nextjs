14b6e666364c8181feb23a355e4008e0b08523eb
Sami Ahvenniemi <spahvenn@gmail.com>
2023-03-09 11:17:57 +0200

    Improve front page visuals

--- src/components/PagePreviewBox.tsx
+++ src/components/PagePreviewBox.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Card, CardContent, Typography } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -16,26 +16,30 @@ export function PagePreviewBox({ pagePreviewInfo, priority }: Props) {
   const { src, title, description, btnUrl } = pagePreviewInfo;
   return (
     <Box mb={{ md: 2, lg: 0 }}>
-      <Link href={btnUrl}>
-        <Image
-          alt={title}
-          src={src}
-          width={4032}
-          height={2268}
-          priority={priority}
-          sizes="(max-width: 599px) 100vw, 
+      <Card>
+        <Link href={btnUrl}>
+          <Image
+            alt={title}
+            src={src}
+            width={4032}
+            height={2268}
+            priority={priority}
+            sizes="(max-width: 599px) 100vw, 
                 (max-width: 1199px) 50vw,
                 270px"
-          style={{ width: "100%", height: "100%" }}
-        />
-      </Link>
-      <Typography variant="h5" mt={0.5} mb={0.5}>
-        {title}
-      </Typography>
-      <Typography mb={1.5}>{description}</Typography>
-      <Button component={Link} href={btnUrl} variant="contained" fullWidth>
-        Browse the collection
-      </Button>
+            style={{ display: "block", width: "100%", height: "100%" }}
+          />
+        </Link>
+        <CardContent>
+          <Typography variant="h5" gutterBottom>
+            {title}
+          </Typography>
+          <Typography mb={1.5}>{description}</Typography>
+          <Button component={Link} href={btnUrl} variant="contained" fullWidth>
+            Browse the collection
+          </Button>
+        </CardContent>
+      </Card>
     </Box>
   );
 }
